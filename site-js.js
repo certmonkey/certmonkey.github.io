aaef229dfa2aa62d78f83b87e48f740b05c15bb8
certmonkey <140364824+certmonkey@users.noreply.github.com>
2023-08-19 19:40:06 +0900

    add new site files for V.1.1.3

---
+++ site-js.js
@@ -0,0 +1,40 @@
+function open_feat() {
+    window.open("https://www.youtube.com/playlist?list=PLr_Omx_QTlEAnP8l2jgUcQ1VNVkGiQe38")
+}
+
+function open_disc() {
+    window.open("https://discord.com/invite/tTukuercMe")
+}
+
+function open_yt() {
+    window.open("https://www.youtube.com/@certmonkey")
+}
+
+function open_tiktok() {
+    window.open("https://www.tiktok.com/@sircheezy.yt")
+}
+
+function open_twitch() {
+    window.open("https://www.twitch.tv/monkestreamz")
+}
+
+function open_twitter() {
+    window.open("https://twitter.com/sircheezyyt")
+}
+
+function open_ig() {
+    window.open("https://instagram.com/certmonkeyyt")
+}
+
+function open_sponsor1() {
+    window.open("https://mandegames.com")
+}
+
+function open_sponsor2() {
+    window.open("https://discord.com/invite/tTukuercMe")
+}
+
+function open_sponsor3() {
+    window.open("https://instagram.com/certmonkeyyt")
+}
+
